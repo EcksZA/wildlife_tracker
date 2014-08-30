0bdb317dbe76dab0ea7a193292341b3ae820f005
Xolani <xmfuko@gmail.com>
2014-08-29 17:14:13 -0700

    can't even explain this commit. If it is screwed up, refer and revert to previous commit

--- app/controllers/sighting_controller.rb
+++
@@ -1,7 +0,0 @@
-class SightingController < ApplicationController
-  def new
-    species = Species.find(:sighting_id)
-    @sighting = species.sightings.new
-    render('sightings/new.html.erb')
-  end
-end
