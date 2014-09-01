387456751e9951f51ab940b72a3be903c41fc056
Xolani Mfuko <xmfuko@gmail.com>
2014-08-31 22:04:37 -0700

    test for validation of the name of a region

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829165659) do
+ActiveRecord::Schema.define(version: 20140901045938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "regions", force: true do |t|
+    t.string "name"
+  end
 
   create_table "sightings", force: true do |t|
     t.date    "date"
